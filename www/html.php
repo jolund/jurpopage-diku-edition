e34798f28b2295f9d763bb91d1a55a19d12059a4
jolund <johannes.lund@gmail.com>
2011-05-06 06:53:46 -0700

    Removed use of 'extract()' function. Replaced use of standard mysql functions with mysqli and prepared statement. No longer posible to SQL inject or code inject into '$webpg_title' variable

--- www/html.php
+++ www/html.php
@@ -1,30 +1,39 @@
-<?php
-
+<?php 
 /**
  * Gets core libraries and defines some variables
  */
 require_once './library/common.php';
 require_once './library/configuration.php';
 
+//if(is_array($HTTP_GET_VARS)) extract($HTTP_GET_VARS,EXTR_SKIP);
+
+
+// Get input variables
+// what 'coded' is used for is unknown. Leave it be for now.
+if (isset($HTTP_GET_VARS['coded'])) {
+	parse_str(decode($coded));
+}
+$inpid = $HTTP_GET_VARS['id'];
+$inpid = intval($inpid);
+
+// db_user,db_passwd and db_name from configuration.php
+$mysqli = new mysqli('localhost',$db_user,$db_passwd,$db_name);
+
+if (!$inpid) $inpid = 1; // set default value
+$stmt = $mysqli->prepare("SELECT * FROM page WHERE page_id=? limit 1");
+$stmt->bind_param('d', $inpid); 
+$stmt->execute();
+$stmt->bind_result($pg_id, $webpg_title);
+
+$stmt->fetch();
+$stmt->close();		// close stmt
+$mysqli->close(); 	// close connection to db
+
+// build page stuff
+
 $conn_id = connect();
-
-if(is_array($HTTP_GET_VARS)) extract($HTTP_GET_VARS,EXTR_SKIP);
-parse_str(decode($coded));
-
-if(empty($id)) $id=1;
-
 $web = new speed_template($template_path);
 $web->register($template_name);
-
-$query = "
-SELECT *
-FROM webpg 
-WHERE webpg_id='$id'
-";
-$result = fn_query($conn_id,$query);
-while($rows = fn_fetch_array($result)) extract($rows,EXTR_OVERWRITE);
-//$page_content = nl2br($page_content);
-
 $web->parse($template_name);
 $web_content = $web->return_template($template_name);
 disconnect($conn_id);
